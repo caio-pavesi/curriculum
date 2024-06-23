703391d69da17980c34a99ae925ab4a1f6fd3796
caio-pavesi <ocaiopavesi@hotmail.com>
2024-06-23 11:56:10 -0300

    docstrigns to english

--- app/main.py
+++ app/main.py
@@ -1,5 +1,1 @@
-"""
-Aqui devem estar todos os códigos que serão executados quando o programa for iniciado.
-Este arquivo é o ponto de entrada do programa, então crie módulos para cada funcionalidade
-e importe-os aqui.
-"""
+"""Main file for the application. This is where the application starts."""
---
+++ app/source/__init__.py
@@ -0,0 +1,1 @@
+"""The source folder contains the modules for the application."""
